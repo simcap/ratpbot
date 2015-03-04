a5a0ad4dc545b5ddcd504b85ec5e9a52db145429
simcap <simcap@fastmail.com>
2015-03-04 21:54:51 +0100

    css work; introducing some structs

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"regexp"
 	"text/template"
 )
 
@@ -18,10 +20,37 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 
 func api(w http.ResponseWriter, r *http.Request) {
-	question := r.URL.Query().Get("q")
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+
+	question := &Question{text: r.URL.Query().Get("q")}
+	answer := process(question)
 	json.NewEncoder(w).Encode(map[string]interface{}{
-		"answer":   "Good question!",
-		"question": question,
+		"answer":   answer.text,
+		"question": question.text,
 	})
+}
+
+type Question struct {
+	text string
+}
+
+type Answer struct {
+	text string
+}
+
+func process(q *Question) Answer {
+	text := q.text
+	metroEnquiry := regexp.MustCompile("\\b[123456789]\\b{1}|\\b10|11|12|13|14\\b")
+	rerEnquiry := regexp.MustCompile("\\b(?:[Rr][Ee][Rr])?([ABCDE]){1}\\b")
+
+	metroMatch := metroEnquiry.FindStringSubmatch(text)
+	if metroMatch != nil {
+		return Answer{text: fmt.Sprintf("Ligne %s", metroMatch[1])}
+	}
+
+	rerMatch := rerEnquiry.FindStringSubmatch(text)
+	if rerMatch != nil {
+		return Answer{text: fmt.Sprintf("RER %s", rerMatch[1])}
+	}
+	return Answer{text: "Hmmm..."}
 }
